f8de18716d43034d5d9261f6871b3e14102c7bfd
nech <sowul789@gmail.com>
2023-07-11 15:53:38 +0300

    dasdca

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Router from "./Router";
 
 function App() {
+  useEffect(() => {
+    const win: any = window;
+
+    win.yaContextCb.push(() => {
+      /* @ts-ignore */
+      Ya.Context.AdvManager.renderWidget({
+        renderTo: "yandex_rtb_C-A-2507683-1",
+        blockId: "C-A-2507683-1",
+      });
+    });
+  }, []);
   return (
     <div>
+      Ads
+      <div id="yandex_rtb_C-A-2507683-1"></div>
       <Router />
     </div>
   );
